f5aff80e47b4bb274d1ff46e6d7464811f6de98e
naffahshah <naffah70@gmail.com>
2025-07-08 22:40:03 +0500

    removed auth from oilChange APIs

--- routes/oilChange.js
+++ routes/oilChange.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const oilChangeController = require('../controllers/oilChangeController');
 const auth = require('../middleware/auth');
 
-router.post('/add', auth, oilChangeController.addOilChange);
-router.get('/vehicle/:carId', auth, oilChangeController.getVehicleOilChanges);
+router.post('/add', /*auth,*/ oilChangeController.addOilChange);
+router.get('/vehicle/:carId', /*auth,*/ oilChangeController.getVehicleOilChanges);
 
 module.exports = router;
